84244e61407c9c592b05006bcc05b93a9f854592
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2021-09-04 11:56:16 +0430

    change tree structue of header

--- layout/Header/index.jsx
+++ layout/Header/index.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import headerStyles from './header.module.scss';
 import styles from '../../assets/styles/styles.webflow.module.scss';
 import Navbar from '../../components/Navbar';
+import logoPng from '../../assets/images/603df7f309bc103be0e8a8fb_icon.png';
 const Header = () => {
   return (
-    <div className={headerStyles['navigation-desktop-wrapper']}>
-      <div className={styles['_1200-container']}>
-        <div className={headerStyles['navigation-desktop-content-flex']}>
-          <div className={headerStyles['navigation-desktop-left-wrap']}>
-            <img
-              src="/assets/images/603df7f309bc103be0e8a8fb_icon.png"
-              width={49}
-              alt="logo"
-              className="ir-china-logo-nav"
-            />
+    <div className={`${styles['section']} wf-section`}>
+      <div className={headerStyles['navigation-desktop-wrapper']}>
+        <div className={styles['_1200-container']}>
+          <div className={headerStyles['navigation-desktop-content-flex']}>
+            <div className={headerStyles['navigation-desktop-left-wrap']}>
+              <img src={logoPng} width={49} alt="logo" className="ir-china-logo-nav" />
+            </div>
+            <Navbar className={headerStyles['navigation-right-wrap']} />
           </div>
-          <Navbar className={headerStyles['navigation-right-wrap']} />
         </div>
       </div>
     </div>
