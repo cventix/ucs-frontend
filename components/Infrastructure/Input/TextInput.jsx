ec3456bd9153a90a6b8fdeedf12429269b6f9d20
mehdi.a <mahdiabaszadeh1995@gmail.com>
2021-09-07 20:02:12 +0430

    set .md for components

--- components/Infrastructure/Input/TextInput.jsx
+++ components/Infrastructure/Input/TextInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Controller, useFormContext } from 'react-hook-form';
 import { FormControl, FormLabel } from 'react-bootstrap';
 import styles from './textInput.module.scss';
@@ -41,4 +42,34 @@ const TextInput = ({ name, id, inputClassName, labelClassName, labelText, type, 
   );
 };
 
+TextInput.propTypes = {
+  id: PropTypes.string,
+  value: PropTypes.string,
+  ref: PropTypes.func,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  className: PropTypes.string,
+  maxLength: PropTypes.number,
+  labelClassName: PropTypes.string,
+  labelText: PropTypes.string,
+  hasExtraTag: PropTypes.bool,
+  extraTag: PropTypes.node,
+};
+TextInput.defaultProps = {
+  id: '',
+  value: '',
+  ref: () => {},
+  onChange: () => {},
+  onBlur: () => {},
+  type: 'text',
+  name: 'input',
+  className: '',
+  maxLength: 50,
+  labelClassName: '',
+  labelText: 'input',
+  hasExtraTag: false,
+  extraTag: null,
+};
 export default TextInput;
