b39187d3f715dcc08455601e18be0aba071ea11e
mehdi.a <mahdiabaszadeh1995@gmail.com>
2021-09-09 16:46:09 +0430

    update carousel and fix bugs

--- components/Hybrid/Carousel/index.jsx
+++ components/Hybrid/Carousel/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
-import Link from 'next/link';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import sliderStyles from './carousel.module.scss';
+import styles from '../../../assets/styles/styles.webflow.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 const Carousel = (props) => {
-  const items = props.items;
+  const { children, title, titleClassName } = props;
   const settings = {
     ...props.settings,
     arrows: true,
@@ -17,12 +17,20 @@ const Carousel = (props) => {
 
   return (
     <div className={sliderStyles['slider-wrapper']}>
+      {title ? (
+        <h1 className={titleClassName && titleClassName}>
+          <span className={styles['text-span-6']}>{title && title}</span>
+        </h1>
+      ) : (
+        <div className="mt-5" />
+      )}
+
       <div className={sliderStyles['arrows']}>
         <button
           className={sliderStyles['button-arrow']}
           onClick={() => {
             const element = document.getElementsByClassName('slick-arrow slick-prev')[0];
-            element.click();
+            element?.click();
           }}
         >
           <FontAwesomeIcon className={sliderStyles['icon-slider']} icon={faCaretLeft} />
@@ -31,36 +39,21 @@ const Carousel = (props) => {
           className={sliderStyles['button-arrow']}
           onClick={() => {
             const element = document.getElementsByClassName('slick-arrow slick-next')[0];
-            element.click();
+            element?.click();
           }}
         >
           <FontAwesomeIcon className={sliderStyles['icon-slider']} icon={faCaretRight} />
         </button>
       </div>
-      <Slider {...settings}>
-        {items.map((item) => {
-          return (
-            <div className={sliderStyles['slide-item']} key={item.id}>
-              <div className={sliderStyles['img-container']}>
-                <img src={item.imgUrl} alt={item.titleItem} className={sliderStyles['img-slide']} />
-              </div>
-              <div className="content-container">
-                <Link href={item.href}>
-                  <a className={sliderStyles['title-slider-container']}>
-                    <h2 className={sliderStyles['title-slider']}>{item.titleItem}</h2>
-                  </a>
-                </Link>
-                <p className={sliderStyles['desc-slider']}>{item.description}</p>
-              </div>
-            </div>
-          );
-        })}
-      </Slider>
+      <Slider {...settings}>{children}</Slider>
     </div>
   );
 };
 
 Carousel.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string,
+  titleClassName: PropTypes.string,
   settings: {
     dots: PropTypes.bool,
     infinite: PropTypes.bool,
@@ -72,6 +65,8 @@ Carousel.propTypes = {
 };
 
 Carousel.defaultProps = {
+  title: '',
+  titleClassName: '',
   settings: {
     dots: false,
     infinite: false,
