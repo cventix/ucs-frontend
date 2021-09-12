1bbaa314091fbfbc57e166ecea9e2d37cc39350d
mehdi.a <mahdiabaszadeh1995@gmail.com>
2021-09-12 19:44:30 +0430

    create modalpro

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,11 @@
 import Layout from '../layout';
 import React from 'react';
-import Carousel from '../components/Hybrid/Carousel';
-import styles from '../assets/styles/styles.webflow.module.scss';
+import DialogPro from '../components/Hybrid/DialogPro';
 
 export default function Home() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    arrows: true,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-  };
-
   return (
     <Layout>
-      <div style={{ marginTop: '200px' }} />
-      <div style={{ height: '300px', marginTop: '100px', backgroundColor: '#000' }}>
-        <Carousel settings={settings} titleClassName={styles['podcast-heading']}>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-        </Carousel>
-      </div>
+      <DialogPro />
     </Layout>
   );
 }
