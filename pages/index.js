440a18ee1cc4690c5d8b13953fe1400b4a9259e9
mehdi.a <mahdiabaszadeh1995@gmail.com>
2021-09-14 19:19:58 +0430

    fix gallery modal

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import Layout from '../layout';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DialogGallery from '../components/Hybrid/DialogGallery';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
+  const router = useRouter();
+  const { gallery } = router.query;
+  const [openDialogGalley, setOpenDialogGalley] = useState(false);
+
+  useEffect(() => {
+    if (gallery) {
+      setOpenDialogGalley(true);
+    } else {
+      setOpenDialogGalley(false);
+    }
+  }, [router]);
   return (
     <Layout>
-      <DialogGallery />
+      <div style={{ height: '500px', marginTop: '200px' }}>
+        <Link href={{ pathname: '/', query: { gallery: 'test' } }}>
+          <a>path</a>
+        </Link>
+      </div>
+
+      {openDialogGalley && <DialogGallery />}
     </Layout>
   );
 }
