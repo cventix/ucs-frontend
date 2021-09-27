666f021cbd9c0eebc4eaccf7448e93cdc1b5a403
pejman-khanjari <khanjari.p.73@gmail.com>
2021-09-27 11:53:00 +0330

    config react hot toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { Toaster } from 'react-hot-toast';
 import '../assets/styles/normalize.css';
 import '../assets/styles/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,6 +8,7 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster position="top-right" />
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
