ca907430d5595c52cac99707209f1de2402b3892
mehdi.a <mahdiabaszadeh1995@gmail.com>
2021-09-29 17:46:31 +0330

    fetch data of index page

--- components/HomePage/Blogs/index.jsx
+++ components/HomePage/Blogs/index.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Carousel from '../../Hybrid/Carousel';
+import { items, settings } from './items';
 import editorStyle from './Blogs.module.scss';
 import style from '../../../assets/styles/styles.webflow.module.scss';
-import { items, settings } from './items';
+import { useBlogMutation } from '../../../hooks/Query/cms';
 
 const Blogs = () => {
+  const { data: blogResult, isLoading, error } = useBlogMutation();
+  useEffect(() => {
+    error && console.log(error);
+  }, [error]);
   return (
-    <Carousel
-      settings={settings}
-      title="Blogs & Articles"
-      carouselClassName={editorStyle['articles']}
-      titleClassName={style['carousel-heading']}
-      items={items}
-      itemClassName="medium-square"
-      linkType="blog"
-    />
+    <>
+      {isLoading ? (
+        'please wait...'
+      ) : (
+        <Carousel
+          settings={settings}
+          title="Blogs & Articles"
+          carouselClassName={editorStyle['articles']}
+          titleClassName={style['carousel-heading']}
+          items={items(blogResult?.data)}
+          itemClassName="medium-square"
+          linkType="blog"
+        />
+      )}
+    </>
   );
 };
 
